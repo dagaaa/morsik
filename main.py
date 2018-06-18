cf1d8c4bcb32177b0ca2b833213c7661d2cd4a79
dagaaa <dagmaragoj@gmail.com>
2018-06-18 10:44:14 +0200

    tfifdf in new file

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from search_engine import SearchEngine
 from preprocess import Preprocessor, WithUrlPreprocessor
 from gensim.models import TfidfModel
 from gensim.corpora import Dictionary
-
+from tfidf_model import *
+tfidf= TFIDF(save=True)
 # lda = LDA.with_url_handling(max_workers=4, save=True, num_topics=200)
 # # jsons = parse_dir_json('/home/robert/PycharmProjects/mors_crawler-master/mors_crawler/data')
 jsons = parse_dir_json('C:\\Users\\dagmara\\Desktop\\mobileworld\\data', limit=100)
 urls, documents = zip(*jsons)
-preprocessor = Preprocessor()
-preprocessed_docs = preprocessor.process_docs(documents)
+# preprocessor = Preprocessor()
+# preprocessed_docs = preprocessor.process_docs(documents)
 
-dct = Dictionary(preprocessed_docs)
-corpus = [dct.doc2bow(line) for line in preprocessed_docs]  # convert dataset to BoW format
-model = TfidfModel(corpus)  # fit model
-vector = model[corpus[0]]  # apply model
-
-searchEngine = SearchEngine(tfidf_model=model, dictionary=dct)
+# dct = Dictionary(preprocessed_docs)
+# corpus = [dct.doc2bow(line) for line in preprocessed_docs]  # convert dataset to BoW format
+# model = TfidfModel(corpus)  # fit model
+# vector = model[corpus[0]]  # apply model
+model, dict = tfidf.train(documents)
+searchEngine = SearchEngine()
 #
 # yy = searchEngine.infer_all(jsons)
 print(len(searchEngine.dictionary))
